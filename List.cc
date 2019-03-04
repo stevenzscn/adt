63ff6a3de6d233e63bdb15096a3dc24d9f902bb8
Steven Zhang <stevenzscn@gmail.com>
2019-03-04 19:33:30 +0800

    add List

---
+++ List.cc
@@ -0,0 +1,14 @@
+#include "List.h"
+#include <iostream>
+
+using namespace std;
+
+void PrintList(const ListNode *head) {
+    if (head != nullptr) {
+        const ListNode *node = head;
+        while (node != nullptr) {
+            cout << node->value << " ";
+            node = node->next;
+        }
+    }
+}
---
+++ List.h
@@ -0,0 +1,6 @@
+struct ListNode {
+    int value = 0;
+    ListNode *next = nullptr;
+};
+
+void PrintList(const ListNode *head);
